d45cc807a4a77104a00582354c04a78b0a8a4d9c
Quynh Ngan Dao <nguy2884@umn.edu>
2023-06-22 23:06:00 -0500

    set up server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(express.static('server/public'));
+
+let tasksRouter = require('./routes/tasks');
+app.use('/tasks', tasksRouter);
+
+app.listen(PORT, () => {
+    console.log('up and running on port', PORT)
+});
