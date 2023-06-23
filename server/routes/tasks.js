235f4a4b98c2045dd6787e054c06044a2c67ef7d
Quynh Ngan Dao <nguy2884@umn.edu>
2023-06-23 18:28:20 -0500

    set up GET req

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,18 +1,33 @@
-const express = require('express');
-const pool = require('../modules/pool');
-const router = express.Router();
+const express = require("express");
+const pool = require("../modules/pool");
+const tasksRouter = express.Router();
 
-// GET 
+// GET
+tasksRouter.get("/", (req, res) => {
+  // set queryText
+  let queryText = 'SELECT * FROM "tasks";';
+
+  // use pool
+  pool
+    .query(queryText)
+    .then((result) => {
+      // send data to client
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log("Error getting data", error);
+      res.sendStatus(500);
+    });
+});
+
 
 // POST
 
-// PUT 
+// PUT
 
-// DELETE 
+// DELETE
 
+// ORDER
 
-// ORDER 
-
-
-// exporting router for server to use 
+// exporting router for server to use
 module.exports = tasksRouter;
