f10f9de5f2e681b8ea54ce39fb4d417b8a5ee7d0
Quynh Ngan Dao <nguy2884@umn.edu>
2023-06-23 16:52:52 -0500

    added comments to server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
-const express = require('express');
+// require dependencies
+const express = require("express");
 const bodyParser = require("body-parser");
+let tasksRouter = require("./routes/tasks");
 
-const app = express();
-
+// PORT
 const PORT = 5000;
 
-app.use(bodyParser.urlencoded({ extended: true}));
+// use express
+const app = express();
+// use body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+// serve static file
+app.use(express.static("server/public"));
+// use router
+app.use("/tasks", tasksRouter);
 
-app.use(express.static('server/public'));
-
-let tasksRouter = require('./routes/tasks');
-app.use('/tasks', tasksRouter);
-
+// Start server
 app.listen(PORT, () => {
-    console.log('up and running on port', PORT)
+  console.log("up and running on port", PORT);
 });
