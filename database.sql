cb5ba28d17bc91177b34c88c4071ce7a6dfd2e88
Quynh Ngan Dao <nguy2884@umn.edu>
2023-06-24 15:35:04 -0500

    no changes

--- database.sql
+++ database.sql
@@ -2,9 +2,8 @@ CREATE TABLE "tasks" (
     "id" serial primary key,
     "note" varchar(800),
     "complete" boolean not null default false
-    "priority"  boolean not null default false
 );
 
-INSERT INTO "tasks" ("note", "complete", "priority")
-VALUES ('Wash the dishes', false, false), 
-('Finish weekend challenge', false, true);
+INSERT INTO "tasks" ("note", "complete")
+VALUES ('Wash the dishes', false), 
+('Finish weekend challenge', false);
