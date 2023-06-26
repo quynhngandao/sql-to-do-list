58f0615bb5624e27711e2b54cebd18ff65648ea6
Quynh Ngan Dao <nguy2884@umn.edu>
2023-06-26 16:15:36 -0500

    edited PORT to deploy Heroku

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 let tasksRouter = require("./routes/tasks");
 
 // PORT
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.DATABASE_URL
 
 // use express
 const app = express();
