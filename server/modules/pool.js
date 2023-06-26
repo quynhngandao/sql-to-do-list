5a525e8c441ab384d348a33477febf227fe5f4b9
Quynh Ngan Dao <nguy2884@umn.edu>
2023-06-26 16:20:10 -0500

    trying to deploy heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,11 +1,21 @@
 // require pg
 const pg = require('pg');
 // set up pg to connect to database 
-const pool = new pg.Pool({
+let pool; 
+
+if (process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL, 
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
+}else {
+    pool = new pg.Pool({
     database: 'weekend-to-do-app',
     host: 'localhost',
     port: 5432
 });
-
+};
 // exporting pool for use in server
 module.exports = pool
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 let tasksRouter = require("./routes/tasks");
 
 // PORT
-const PORT = process.env.DATABASE_URL
+const PORT = process.env.PORT || 5000;
 
 // use express
 const app = express();
